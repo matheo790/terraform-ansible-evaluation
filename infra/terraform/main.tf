5faf91487238481eeab819333dd40d6b1b3c6c15
BRAOX <matheo.charot@gmail.com>
2026-02-02 13:58:37 +0100

    Correct conflict

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,3 +5,19 @@
 # - docker_container nginx (image nginx:alpine) + config reverse proxy
 #
 # Hint: vous pouvez monter un fichier de conf nginx (host_path -> container_path)
+
+resource "docker_network" "evaluation_network" {
+  name = "evaluation_network"
+}
+
+resource "docker_image" "app_image" {
+  name = "tp-app:latest"
+}
+
+
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,2 +1,7 @@
 # TODO: outputs utiles
 # output "url" { value = "http://localhost:8080" }
+
+output "url" {
+  value = "http://localhost:8080/health"
+}
+
