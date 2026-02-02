646926b11b6d1eb5ab0990b401d08ed37a2a3122
BRAOX <matheo.charot@gmail.com>
2026-02-02 11:22:24 +0100

    Terraform Ok

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,3 +5,53 @@
 # - docker_container nginx (image nginx:alpine) + config reverse proxy
 #
 # Hint: vous pouvez monter un fichier de conf nginx (host_path -> container_path)
+terraform {
+
+}
+
+resource "docker_network" "evaluation_network" {
+  name = "evaluation_network"
+}
+
+resource "docker_image" "app_image" {
+  name = "tp-app:latest"
+}
+
+resource "docker_container" "app" {
+  name  = "evaluation_container"
+  image = docker_image.app_image.name
+
+  networks_advanced {
+    name = docker_network.evaluation_network.name
+  }
+
+  env = [
+    "APP_ENV=prod",
+    "PORT=5000"
+  ]
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "evaluation_nginx"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+
+  networks_advanced {
+    name = docker_network.evaluation_network.name
+  }
+
+  volumes {
+    host_path      = abspath("${path.module}/nginx.conf")
+    container_path = "/etc/nginx/conf.d/default.conf"
+    read_only      = true
+  }
+
+}
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,2 +1,14 @@
 # TODO: outputs utiles
 # output "url" { value = "http://localhost:8080" }
+
+output "url" {
+  value = "http://localhost:8080/health"
+}
+
+output "evaluation_container" {
+  value = docker_container.app.name
+}
+
+output "nginx_container" {
+  value = docker_container.nginx.name
+}
